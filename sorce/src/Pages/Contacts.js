0843a7c482e49d2a2eb791002b187a9faf70cd07
Владислав <130240871+user-F13@users.noreply.github.com>
2023-06-16 15:32:18 +0300

    Update Contacts.js
    Добавление формы обратной связи для пользователей.

--- sorce/src/Pages/Contacts.js
+++ sorce/src/Pages/Contacts.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react'
+import { Form, Button, Container, FormLabel} from 'react-bootstrap'
 
 export default class Contacts extends Component {
     render() {
         return (
-            <div>
-                Contacts
-            </div>
+            <Container>
+                <h1> Contact us</h1>
+                <Form>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email Address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email"/>
+                    <Form.Text>
+                        Тут можно обратиться к нам.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group controlId='formBasicPassword'>
+                    <Form.Label>Example textarea</Form.Label>
+                    <Form.Control as="textarea" rows="3" />
+                </Form.Group>
+
+                <Form.Group controlId='formBasicCheckbox'>
+                    <Form.Check type="checkbox" label="Check me out"/>
+                </Form.Group>
+                <Button variant='primary' type="submit">Submit</Button>
+
+                </Form>
+            </Container>
         )
     }
 }
