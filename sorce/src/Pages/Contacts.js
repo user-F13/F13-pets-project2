9d3def2a7a1eb34fb79ed4d769eff679e21c97a9
Владислав <130240871+user-F13@users.noreply.github.com>
2023-06-22 20:15:05 +0300

    Update Contacts.js

--- sorce/src/Pages/Contacts.js
+++ sorce/src/Pages/Contacts.js
@@ -1,32 +1,33 @@
 import React, { Component } from 'react'
-import { Form, Button, Container, FormLabel} from 'react-bootstrap'
+import { Form, Button, Container } from 'react-bootstrap'
 
 export default class Contacts extends Component {
     render() {
         return (
-            <Container>
-                <h1> Contact us</h1>
+            <Container className='mt-5'>
+                <h1> Задайте Ваш вопрос</h1>
                 <Form>
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Email Address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email"/>
-                    <Form.Text>
-                        Тут можно обратиться к нам.
-                    </Form.Text>
-                </Form.Group>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Почтовый адрес</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email"/>
+                        <Form.Text>
+                            Тут можно обратиться к нам.
+                        </Form.Text>
+                    </Form.Group>
 
-                <Form.Group controlId='formBasicPassword'>
-                    <Form.Label>Example textarea</Form.Label>
-                    <Form.Control as="textarea" rows="3" />
-                </Form.Group>
+                    <Form.Group controlId='formBasicPassword'>
+                        <Form.Label>Введите текст сообщения</Form.Label>
+                        <Form.Control as="textarea" rows="3" />
+                    </Form.Group>
 
-                <Form.Group controlId='formBasicCheckbox'>
-                    <Form.Check type="checkbox" label="Check me out"/>
-                </Form.Group>
-                <Button variant='primary' type="submit">Submit</Button>
+                    <Form.Group controlId='formBasicCheckbox'>
+                        <Form.Check type="checkbox" label="Check me out"/>
+                    </Form.Group>
+                    <Button variant='primary' type="submit">Отправить</Button>
 
                 </Form>
             </Container>
+
         )
     }
 }
