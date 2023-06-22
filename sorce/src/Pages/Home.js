ac5825734c5dfc89e6b7eeef360577286db4f379
Владислав <130240871+user-F13@users.noreply.github.com>
2023-06-22 20:15:30 +0300

    Update Home.js

--- sorce/src/Pages/Home.js
+++ sorce/src/Pages/Home.js
@@ -1,59 +1,90 @@
 import React, { Component } from 'react';
 import CarouselBox from '../Components/CarouselBox';
-import { Container, CardGroup, Card, Button } from 'react-bootstrap';
+import { Container, CardGroup, Card } from 'react-bootstrap';
 
 export default class Home extends Component {
     render() {
         return (
             <>
-            <CarouselBox />
-            <Container>
-                <h2 className="text-center m-4">Our team </h2>
-                <CardGroup>
-                    <Card className="p-3">
-                        <Card.Img variant='top'
-                        src='https://sun9-36.userapi.com/s/v1/ig2/4OIQJZS-sgO5TOku-bGJg8_E2PZjyIZNjBQzWl5d5BWrbyC_QLXoeRR8VOz4umuyyMJ--4jZtSa8-CRvFZF3Afkq.jpg?size=400x508&quality=96&crop=32,32,716,910&ava=1'
-                        />
-                        <Card.Body>
-                            <Card.Title>Developers</Card.Title>
-                            <Card.Text>
-                                Это наш самый главный разработчик. Он крутой.
-                            </Card.Text>
-                           <div id="browse_app">
-                                <a class="btn btn-large btn-info" href="https://www.youtube.com/">Browse</a>
-                            </div>
-                        </Card.Body>
-                    </Card>
-                    <Card className="p-3">
-                        <Card.Img variant='top'
-                        src='https://sun9-36.userapi.com/s/v1/ig2/4OIQJZS-sgO5TOku-bGJg8_E2PZjyIZNjBQzWl5d5BWrbyC_QLXoeRR8VOz4umuyyMJ--4jZtSa8-CRvFZF3Afkq.jpg?size=400x508&quality=96&crop=32,32,716,910&ava=1'
-                        />
-                        <Card.Body>
-                            <Card.Title>Developers</Card.Title>
-                            <Card.Text>
-                                Это наш самый главный разработчик. Он крутой.
-                            </Card.Text>
-                            <div id="browse_app">
-                                <a class="btn btn-large btn-info" href="https://www.youtube.com/">Browse</a>
-                            </div>
-                        </Card.Body>
-                    </Card>
-                    <Card className="p-3">
-                        <Card.Img variant='top'
-                        src='https://sun9-36.userapi.com/s/v1/ig2/4OIQJZS-sgO5TOku-bGJg8_E2PZjyIZNjBQzWl5d5BWrbyC_QLXoeRR8VOz4umuyyMJ--4jZtSa8-CRvFZF3Afkq.jpg?size=400x508&quality=96&crop=32,32,716,910&ava=1'
-                        />
-                        <Card.Body>
-                            <Card.Title>Developers</Card.Title>
-                            <Card.Text>
-                                Это наш самый главный разработчик. Он крутой.
-                            </Card.Text>
-                           <div id="browse_app">
-                                <a class="btn btn-large btn-info" href="https://www.youtube.com/">Browse</a>
-                            </div>
-                        </Card.Body>
-                    </Card>
-                </CardGroup>
-            </Container>
+                <CarouselBox />
+                <Container>
+                    <h2 className="text-center m-4">Наши друзья </h2>
+                    <CardGroup>
+                        <Card className="p-3">
+                            <Card.Img variant='top'
+                                      src='https://sun9-61.userapi.com/impg/IuLn-NrOZ95t6m0sS0HY1ZF-GDpWryyrEoOwpg/OIFwrjDaKeA.jpg?size=150x150&quality=96&sign=85512279cde44b159f052c43cadda7a9&type=album'
+                            />
+                            <Card.Body>
+                                <Card.Title>Пушок</Card.Title>
+                                <Card.Text>
+                                    Спокойный и добрый котик
+                                </Card.Text>
+                                <div id="browse_app">
+                                    <a class="btn btn-large btn-info" href="https://www.youtube.com/">Приютить</a>
+                                </div>
+                            </Card.Body>
+                        </Card>
+                        <Card className="p-3">
+                            <Card.Img variant='top'
+                                      src='https://sun9-15.userapi.com/impg/8XdxpSr_wNcyXrZfmx2ft0Z_dxdmFUjTZ_oqzw/iNAH9pAYTMA.jpg?size=150x150&quality=96&sign=7a96fc87e5307c6eebf731b206d5ba62&type=album'
+                            />
+                            <Card.Body>
+                                <Card.Title>Лакки</Card.Title>
+                                <Card.Text>
+                                    Очень дружелюбный и любит детей
+                                </Card.Text>
+                                <div id="browse_app">
+                                    <a class="btn btn-large btn-info" href="https://www.youtube.com/">Приютить</a>
+                                </div>
+                            </Card.Body>
+                        </Card>
+                        <Card className="p-3">
+                            <Card.Img variant='top'
+                                      src='https://sun9-4.userapi.com/impg/j7LlPTJ7WDsnz6sKt-zs8SjHmYFKJUkzRnXAWA/SbtLSqu0fkc.jpg?size=150x150&quality=96&sign=f0ec7505ee42ab0d847216433c5dbd31&type=album'
+                            />
+                            <Card.Body>
+                                <Card.Title>Рекс</Card.Title>
+                                <Card.Text>
+                                    Активный, любит игры на свежем воздухе
+                                </Card.Text>
+                                <div id="browse_app">
+                                    <a class="btn btn-large btn-info" href="https://www.youtube.com/">Приютить</a>
+                                </div>
+                            </Card.Body>
+                        </Card>
+
+                        <Card className="p-3">
+                            <Card.Img variant='top'
+                                      src='https://sun9-34.userapi.com/impg/TD-KKOSMlkmyHS5SzCKxxbOh00g_mg7vqs64fA/sFDsBdBZxNY.jpg?size=150x150&quality=96&sign=95cf1503b462a2c7f7d647b5a427bf6d&type=album'
+                            />
+                            <Card.Body>
+                                <Card.Title>Мелисса</Card.Title>
+                                <Card.Text>
+                                    Грациозная и изящная кошечка
+                                </Card.Text>
+                                <div id="browse_app">
+                                    <a class="btn btn-large btn-info" href="https://www.youtube.com/">Приютить</a>
+                                </div>
+                            </Card.Body>
+                        </Card>
+
+                        <Card className="p-3">
+                            <Card.Img variant='top'
+                                      src='https://sun9-27.userapi.com/impg/zLaYzNcBorlyob7Ucm7mm4_IQdCU7If552Eoxw/DmqyQccy-jc.jpg?size=150x150&quality=96&sign=2d2e210d3a07ce896f93f493139c0f94&type=album'
+                            />
+                            <Card.Body>
+                                <Card.Title>Бобик</Card.Title>
+                                <Card.Text>
+                                    Немного неуклюжий, зато очень любвеобильный
+                                </Card.Text>
+                                <div id="browse_app">
+                                    <a class="btn btn-large btn-info" href="https://www.youtube.com/">Приютить</a>
+                                </div>
+                            </Card.Body>
+                        </Card>
+
+                    </CardGroup>
+                </Container>
             </>
         )
     }
