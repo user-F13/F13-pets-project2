58a9d1bbde9cec9ff294b1703107d2b2e1acf512
LonelyMeatball <127410747+LonelyMeatball@users.noreply.github.com>
2023-06-28 01:35:13 +0300

    Update Footer.js

--- sorce/src/Components/Footer/Footer.js
+++ sorce/src/Components/Footer/Footer.js
@@ -3,17 +3,16 @@ import { Form, Button, Container, FormLabel} from 'react-bootstrap'
 
 export function Footer() {
     return (
-        <div className="footer text-center">
-            <p>Наш адрес:</p>
-            <p>Ростов-на-Дону, ул.Зорге, д.28</p>
+        <div className="footer text-left">
+              <div className="footer text-left ">
             <p></p>
-            <p>Режим работы</p>
-            <p>пн-пт 9:00-21:00</p>
-            <p>сб-вс 10:00-21:00</p>
             <p></p>
-            <p>Номер телефона:</p>
-            <p></p>
-            <p>+7 928 195 4777</p>
+
+            <p>Наш адрес: Ростов-на-Дону, ул.Зорге, д.28</p>
+
+            <p>Режим работы: пн-пт 9:00-21:00; сб-вс 10:00-21:00</p>
+
+            <p>Номер телефона: +7 928 195 4777</p>
 
         </div>
     )
